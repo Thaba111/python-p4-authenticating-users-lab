fbf5121f60c3eb005df1fa038c3e857681411eb3
Edith Thaba <writersherry5@gmail.com>
2024-04-13 12:21:59 +0300

    Done with assignment

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+import json
+from models import User
 
 from models import db, Article, User
 
@@ -17,6 +19,43 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+# Login route
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        username = request.json.get('username')
+
+        # Query the database for the user
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            # Set user_id in session to authenticate the user
+            session['user_id'] = user.id
+            return jsonify({'id': user.id, 'username': user.username}), 200
+        else:
+            return jsonify({'message': 'Invalid username'}), 401
+    except Exception as e:
+        return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
+    
+class Logout(Resource):
+    def delete(self):
+        # Remove user_id from session to log out the user
+        session.pop('user_id', None)
+        return '', 204
+
+@app.route('/check_session', methods=['GET'])
+def check_session():
+    try:
+        user_id = session.get('user_id')
+        if user_id:
+            # Query the database for the user using user_id from session
+            user = User.query.get(user_id)
+            if user:
+                return jsonify({'id': user.id, 'username': user.username}), 200
+        return jsonify({}), 401
+    except Exception as e:
+        return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
 
 class ClearSession(Resource):
 
@@ -48,6 +87,9 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+#api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+#api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
